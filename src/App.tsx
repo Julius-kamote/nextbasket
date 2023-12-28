f3687e57cbbfcbeceebaa286d441dffc0a8d4dba
Julius-kamote <punayjulius04@gmail.com>
2023-12-28 22:39:26 +0800

    Disigning Experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AboutUs from "./components/AboutUs";
 import BestSellerProducts from "./components/BestSellerProducts";
 import BestServices from "./components/BestServices";
+import DesigningExperience from "./components/DesigningExperience";
 import Header from "./components/Header";
 import PracticeAdvice from "./components/PracticeAdvice";
 import Section1 from "./components/Section1";
@@ -15,6 +16,7 @@ function App() {
       <BestServices />
       <PracticeAdvice />
       <AboutUs />
+      <DesigningExperience />
     </div>
   );
 }
---
+++ src/components/DesigningExperience.tsx
@@ -0,0 +1,23 @@
+import bg from "../images/bg.png";
+
+function DesigningExperience() {
+  return (
+    <div className="designing-content">
+      <img src={bg} alt="" />
+      <div className="designing-text">
+        <h4>Designing Better Experience</h4>
+        <h1>
+          Problems trying to resolve <br /> the conflict between{" "}
+        </h1>
+        <p>
+          Problems trying to resolve the conflict between the two <br />
+          major realms of Classical physics:
+        </p>
+        <h3>$16.48</h3>
+        <button>Add your call to action</button>
+      </div>
+    </div>
+  );
+}
+
+export default DesigningExperience;
